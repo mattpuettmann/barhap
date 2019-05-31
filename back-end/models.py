1d0681a04c15fda728c5136133b4dc05a2d6ac2c
Matthew Puettmann <mattpuettmann@Matthews-MacBook-Pro.local>
2019-05-31 11:46:40 -0600

    user post and get routes working

--- back-end/models.py
+++ back-end/models.py
@@ -1,16 +1,33 @@
 from peewee import *
 from flask_login import UserMixin
+from flask_bcrypt import generate_password_hash
 
-DATABASE = PostgresqlDatabase('places', user='adminmatt', password='password')
+DATABASE = PostgresqlDatabase('barhap', user='adminmatt', password='password')
 
 class User(UserMixin, Model):
-    id = IntegerField(primary_key=True)
     username = CharField(unique=True)
     email = CharField(unique=True)
+    city = CharField()
+    state = CharField()
     password = CharField()
 
     class Meta:
         database = DATABASE
+
+    @classmethod
+    def create_user(cls, username, email, city, state, password, **kwargs):
+        email = email.lower()
+        try:
+            cls.select().where(
+                (cls.email==email)
+            ).get()
+        except cls.DoesNotExist:
+            user = cls(username=username, email=email, city=city, state=state)
+            user.password = generate_password_hash(password)
+            user.save()
+            return user
+        else:
+            raise Exception("user with that username/email already exists ya dingus!!")
 
 
 
